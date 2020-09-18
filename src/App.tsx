cc5c8bb9beda5431ccd59817ee468aa801f44d15
Gean Lucas <gean.lfpsa@gmail.com>
2020-09-17 22:36:35 -0300

    Created animated header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import FirstAndSecond from './components/FirstAndSecond';
+import Header from './components/Header';
 import { Main, Section, Sticky } from './styles';
 
 import GlobalStyles from './styles/global';
@@ -12,6 +13,8 @@ function App() {
   return (
     <>
       <GlobalStyles />
+
+      <Header />
 
       <Main style={{ height: '1610vh' }}>
         <Section style={{ height: '72.7%' }}>
