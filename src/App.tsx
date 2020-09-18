deac5645ba9a591371a9029ac31a7f30fe380349
Gean Lucas <gean.lfpsa@gmail.com>
2020-09-17 21:39:14 -0300

    Created homepage file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Main, Section, Sticky } from './styles';
+
+import GlobalStyles from './styles/global';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyles />
+
+      <Main style={{ height: '1610vh' }}>
+        <Section style={{ height: '72.7%' }}>
+          <Sticky className="first"/>
+          <Sticky className="second"/>
+        </Section>
+
+        <Section>
+          <Sticky className="third" style={{ height: '9.7%' }}/>
+        </Section>
+
+        <Section>
+          <Sticky className="fourth" style={{ height: '9.7%' }}/>
+        </Section>
+      </Main>
+    </>
   );
 }
 
---
+++ src/styles/index.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+export const Main = styled.main``;
+
+export const Section = styled.div``;
+
+export const Sticky = styled.div``;
