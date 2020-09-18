d79175b5f4e308d1239b0a5b28541498e8127488
Gean Lucas <gean.lfpsa@gmail.com>
2020-09-17 22:30:52 -0300

    Elements inside component easing out on mouse scroll

--- src/components/FirstAndSecond/index.tsx
+++ src/components/FirstAndSecond/index.tsx
@@ -61,6 +61,12 @@ const First: React.FC = () => {
     ['58vh', '0vh']
   );
 
+  const offsetY = useTransform(
+    scrollYProgress,
+    [0.328, 0.397, 0.461, 0.53],
+    ['0%', '-100%', '-100%', '-200%']
+  )
+
   return (
     <Sticky className="first"
       style={{
@@ -68,27 +74,34 @@ const First: React.FC = () => {
         borderRadius: firstRadius,
       }}
     >
-      <div className="a">
-        <motion.div 
-          className="left-side"
-          style={{
-            height: leftSideHeight
-          }}
-        />
-
-        <div className="right-side">
+      <motion.div 
+        className="offset"
+        style={{
+          y: offsetY
+        }}
+      >
+        <div className="a">
           <motion.div 
-            className="right-image"
+            className="left-side"
             style={{
-              y: rightSideY,
-              scale: rightSideScale,
+              height: leftSideHeight
             }}
           />
-        </div>
 
-      </div>
-      <div className="b"></div>
-      <div className="c"></div>
+          <div className="right-side">
+            <motion.div 
+              className="right-image"
+              style={{
+                y: rightSideY,
+                scale: rightSideScale,
+              }}
+            />
+          </div>
+
+        </div>
+        <div className="b"></div>
+        <div className="c"></div>
+      </motion.div>
     </Sticky>
   )
 }
